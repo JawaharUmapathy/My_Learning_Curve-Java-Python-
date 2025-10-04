6973c0a61f1848b687e008600dc63fcafb8b5982
Jawahar_U <103322098+JawaharUmapathy@users.noreply.github.com>
2025-10-04 12:29:01 +0530

    Delete Basics directory

--- Basics/pp.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class pp {
-    public static void main(String[] args) {
-        Scanner d = new Scanner(System.in);
-        int x = d.nextInt();
-        System.out.print(x*(Math.pow(4,x)));
-    }
-}
